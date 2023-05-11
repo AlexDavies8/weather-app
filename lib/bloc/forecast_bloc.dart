b1958166e110bb08c9fff226aeee839a15c3cd9b
Alex Davies <alex@frogtrade.com>
2023-05-11 15:44:45 +0100

    Added Forecast Bloc and MainPage view

--- lib/bloc/forecast_bloc.dart
+++ lib/bloc/forecast_bloc.dart
@@ -4,10 +4,25 @@ import 'package:weather_app/bloc/bloc.dart';
 import 'package:weather_app/models/pollen_data.dart';
 
 class ForecastBloc extends Bloc {
+  final StreamController _forecastController = StreamController<String?>();
+  Sink<String?> get forecast => _forecastController.sink as Sink<String?>;
+  
+  late Stream<List<PollenData>?> forecastStream;
+
+  ForecastBloc() {
+    forecastStream = _forecastController.stream
+      .asyncMap((placename) => [
+        PollenData(trees: 50, grasses: 24, weeds: 72),
+        PollenData(trees: 42, grasses: 32, weeds: 56),
+        PollenData(trees: 20, grasses: 36, weeds: 48),
+        PollenData(trees: 29, grasses: 31, weeds: 45),
+        PollenData(trees: 32, grasses: 32, weeds: 48)
+      ]);
+    forecast.add("test");
+  }
+  
   @override
   void dispose() {
-    // TODO: implement dispose
+    _forecastController.close();
   }
-  //final StreamController _forecastController = StreamController<List<PollenData>>();
-  //Sink<List<PollenData>> get forecast => _forecastController.sink;
 }
---
+++ lib/views/MainPage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/bloc/bloc_provider.dart';
+import 'package:weather_app/bloc/forecast_bloc.dart';
+
+import '../models/pollen_data.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override 
+  void initState() {
+    super.initState();
+
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      bloc: ForecastBloc(),
+      child: TodayWidget()
+    );
+  }
+}
+
+class TodayWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final forecastBloc = BlocProvider.of<ForecastBloc>(context);
+    return StreamBuilder<List<PollenData>?>(
+      stream: forecastBloc.forecastStream,
+      builder: (context, snapshot) {
+        return Text(snapshot.data?[0].trees.toString() ?? "No Data");
+      }
+    );
+  }
+
+}

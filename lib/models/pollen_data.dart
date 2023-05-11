0b8bdadc4fc088ab865220033d0bb87f1c640fad
Alex Davies <alex@frogtrade.com>
2023-05-11 15:01:47 +0100

    Added Pollen Data Model

---
+++ lib/models/pollen_data.dart
@@ -0,0 +1,8 @@
+class PollenData {
+  final int trees;
+  final int grasses;
+  final int weeds;
+  final String placename;
+
+  PollenData({required this.placename, required this.trees, required this.grasses, required this.weeds});
+}

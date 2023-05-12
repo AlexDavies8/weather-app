915eeff73d35683d5467a53330e1ab099d08e8ed
Alex Davies <alex@frogtrade.com>
2023-05-12 19:15:56 +0100

    Changed routing solution and added prototype flag in main.dart

---
+++ lib/widgets/shared_axis_page_route.dart
@@ -0,0 +1,25 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+
+class SharedAxisPageRoute extends PageRouteBuilder {
+  SharedAxisPageRoute({required Widget page, required SharedAxisTransitionType transitionType}) : super(
+    pageBuilder: (
+      BuildContext context,
+      Animation primaryAnimation,
+      Animation secondaryAnimation,
+    ) => page,
+    transitionsBuilder: (
+      BuildContext context,
+      Animation<double> primaryAnimation,
+      Animation<double> secondaryAnimation,
+      Widget child,
+    ) {
+      return SharedAxisTransition(
+        animation: primaryAnimation,
+        secondaryAnimation: secondaryAnimation,
+        transitionType: transitionType,
+        child: child,
+      );
+    },
+  );
+}
